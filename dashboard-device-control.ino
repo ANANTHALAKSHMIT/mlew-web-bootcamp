4229f1b7e07bf4adce18d5723988dee63786c7f8
MS <72651078+maddydevgits@users.noreply.github.com>
2024-12-13 19:23:43 +0530

    updating code for arduino

---
+++ dashboard-device-control.ino
@@ -0,0 +1,83 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ESP32Servo.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Purna";
+const char* password = "Galla4446";
+String apiRoute="https://mlew-api-iot.onrender.com/get-devices";
+
+bool isSent=false;
+Servo motor;
+
+void setup() {
+  motor.attach(5);
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+}
+
+void checkDeviceState(String api) {
+  DynamicJsonDocument doc(1024);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(api);
+    int responseCode = http.GET();
+    
+    if (responseCode > 0) {
+      String response = http.getString();
+      //Serial.println("Response: " + response);
+
+      // Deserialize the JSON array
+      DeserializationError error = deserializeJson(doc, response);
+      if (error) {
+        Serial.print("Deserialization failed: ");
+        Serial.println(error.f_str());
+        return;
+      }
+
+      // Access the array
+      JsonArray array = doc.as<JsonArray>();
+      for (JsonObject obj : array) {
+        String deviceId = obj["device_id"];
+        String state = obj["state"];
+
+        if (deviceId == "1") { 
+          Serial.print("Device State: ");
+          Serial.println(state);
+
+          // Perform action based on state
+          if (state == "on") {
+            Serial.println("Device 1 is ON");
+            motor.write(180);
+          } else if (state == "off") {
+            Serial.println("Device 1 is OFF");
+            motor.write(0);
+          }
+          break; // Exit the loop once device_id 1 is found
+        }
+      }
+    } else {
+      Serial.print("HTTP GET failed, error: ");
+      Serial.println(responseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi not connected");
+  }
+}
+
+void loop() { 
+    checkDeviceState(apiRoute);
+    delay(5000);
+}
---
+++ send-data-to-dashboard.ino
@@ -0,0 +1,66 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+
+const char* ssid = "Purna";
+const char* password = "Galla4446";
+String apiRoute="https://mlew-api-iot.onrender.com/store?label=";
+String alertRoute="https://mlew-api-iot.onrender.com/alert?type=";
+
+bool isSent=false;
+DHT dht(23,DHT11);
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+  dht.begin();
+}
+
+void sendDataToDashboard(String api){
+  if(WiFi.status()==WL_CONNECTED){
+    HTTPClient http;
+    http.begin(api);
+    int responseCode=http.GET();
+    if(responseCode>0){
+      String response=http.getString();
+      Serial.println(response);
+      http.end();
+    }
+  }
+}
+void loop() {
+    float h=dht.readHumidity();
+    float t=dht.readTemperature();
+
+    if(isnan(h)||isnan(t)) {
+      Serial.println("DHT Error");
+      return;
+    }
+
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(",Temperature: ");
+    Serial.println(t);
+
+    sendDataToDashboard(apiRoute+"Humidity"+"&value="+String(h));
+    delay(2000);
+    sendDataToDashboard(apiRoute+"Temperature"+"&value="+String(t));
+    delay(2000);    
+
+    if(h>80 && !isSent){
+      sendDataToDashboard(alertRoute+"danger"+"&message=Humidity Out of Range");
+      isSent=true;
+    } 
+    if(h<80){
+      isSent=false;
+    }
+}
